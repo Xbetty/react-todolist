70e6606d73dc3315e9391b594c5ded954066117b
Xbetty <1623290520@qq.com>
2019-08-01 23:55:37 +0800

    'propTypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import PropTypes from 'prop-types';
 class TodoItem extends Component {
     constructor(props){
         super(props);
@@ -11,13 +11,26 @@ class TodoItem extends Component {
         deleteItem(index);
     }
     render() {
-        const { content } = this.props;
+        const { content, testPropTypes } = this.props;
         return (
             <div onClick={this.handleClick}>
                 {/* 子组件接收父组件的传递数据 */}
-                {content}
+                {testPropTypes} : {content}
             </div>
         )
     }
+}
+// 接受外部传递的prop类型进行属性校验
+TodoItem.propTypes = {
+    // 必传值
+    testPropTypes: PropTypes.string.isRequired,
+    // content: PropTypes.string,
+    content: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+// 父组件未传递值给子组件，可以自己在组件中定义默认值
+TodoItem.defaultProps = {
+    testPropTypes: '这是一个默认值'
 }
 export default TodoItem;
